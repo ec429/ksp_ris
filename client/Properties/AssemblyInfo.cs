a75bdc1e03af9ba7ab93ba5679ce05d24aaf7e52
Edward Cree <ec429@cantab.net>
2017-05-30 01:03:46 +0100

    RIS v0.1

--- client/Properties/AssemblyInfo.cs
+++ client/Properties/AssemblyInfo.cs
@@ -16,11 +16,11 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("aaa18268-304c-4d1a-9cee-93d1223ed52c")]
 
-[assembly: AssemblyVersion("0.0")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1.0")]
 
 // Use KSPAssembly to allow other DLLs to make this DLL a dependency in a
 // non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it
 // does not appear to have a hard requirement to match the assembly version.
-[assembly: KSPAssembly("RIS", 0, 0)]
+[assembly: KSPAssembly("RIS", 0, 1)]
 [assembly: KSPAssemblyDependency("ContractConfigurator", 1, 0)]
